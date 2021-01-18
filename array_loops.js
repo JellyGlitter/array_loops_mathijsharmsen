f699cf8599e5684eb8feb4b138224d488e454081
JellyGlitter <75260180+JellyGlitter@users.noreply.github.com>
2021-01-18 21:41:03 +0100

    Update and rename array-loops.js to array_loops.js

--- array_loops.js
+++ array_loops.js
@@ -4,6 +4,13 @@
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
 
+let index;
+for (index=0; index<=3;index++) {
+    names[index] = names [index] +"je";
+}
+
+console.log(names);
+
 // Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
@@ -15,6 +22,13 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // --------------------------
 
 const numbers = [2, 4, 5, 29, 38];
+
+let index1;
+for (index1=0; index1<=4;index1++) {
+    numbers [index1] = numbers [index1] * 3;
+}
+
+console.log(numbers);
 
 // Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
@@ -28,6 +42,12 @@ const numbers = [2, 4, 5, 29, 38];
 
 const figures = [4, 6, 12, 34, 1];
 
+let index2;
+for (index2=0; index2<=4;index2++) {
+    figures[index2] = figures [index2] + 3;
+}
+
+console.log(figures)
 // Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(figures) geeft: [ 7, 9, 15, 37, 4 ]
